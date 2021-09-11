7d016546ddfedb380f94bf26213fc56352ca8e61
Florenz Heldermann <hi@flore.nz>
2021-09-11 20:27:03 +0200

    :wrench: remove noref rel from sponsored link #sellout

--- components/Advertisement.vue
+++ components/Advertisement.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="referer">
    <p v-if="adData.showAmazon && adData.showGenial">
-     <em>Ihr könnt diesen Blog - und gleichzeitig eure Buchhandlung vor Ort - unterstützen in dem ihr ihr dieses Buch bei <a :href="adData.genialokalUrl" target="_blank" rel="noopener noreferrer">Genialokal</a> kauft. Oder finanziert Jeff Bezos's nächsten Weltraumspazierung und klickt diesen <a :href="adData.amazonUrl" target="_blank" rel="noopener noreferrer">Amazonlink</a>.</em>
+     <em>Ihr könnt diesen Blog - und gleichzeitig eure Buchhandlung vor Ort - unterstützen in dem ihr ihr dieses Buch bei <a :href="adData.genialokalUrl" target="_blank" rel="noopener sponsored nofollow">Genialokal</a> kauft. Oder finanziert Jeff Bezos's nächsten Weltraumspazierung und klickt diesen <a :href="adData.amazonUrl" target="_blank" rel="noopener sponsored nofollow">Amazonlink</a>.</em>
    </p>
    <p v-if="adData.showAmazon && !adData.showGenial">
-     <em>Ihr könnt diesen Blog unterstützen in dem ihr ihr dieses Buch bei <a :href="adData.amazonUrl" target="_blank" rel="noopener noreferrer">Amazon</a> kauft. Oder noch besser, besucht eure Buchhandlung um die Ecke. Soll dort auch ganz schön sein.</em>
+     <em>Ihr könnt diesen Blog unterstützen in dem ihr ihr dieses Buch bei <a :href="adData.amazonUrl" target="_blank" rel="noopener nofollow sponsored">Amazon</a> kauft. Oder noch besser, besucht eure Buchhandlung um die Ecke. Soll dort auch ganz schön sein.</em>
    </p>
    <p v-if="!adData.showAmazon && adData.showGenial">
-     <em>Ihr könnt diesen Blog - und gleichzeitig eure Buchhandlung vor Ort - unterstützen in dem ihr ihr dieses Buch bei <a :href="adData.genialokalUrl" target="_blank" rel="noopener noreferrer">Genialokal</a> kauft.</em>
+     <em>Ihr könnt diesen Blog - und gleichzeitig eure Buchhandlung vor Ort - unterstützen in dem ihr ihr dieses Buch bei <a :href="adData.genialokalUrl" target="_blank" rel="noopener nofollow sponsored">Genialokal</a> kauft.</em>
    </p>
   </div>
 </template>
