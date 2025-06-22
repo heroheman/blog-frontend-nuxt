a2b4ea90da7f5cbd48d6e94929530f32ea9a7e48
Florenz Heldermann <f.heldermann@reply.de>
2025-06-22 17:34:14 +0200

    chore: disable eslint-module due to dependency conflicts
    - Commented out '@nuxtjs/eslint-module' in nuxt.config.js
    - Prevents issues arising from conflicting dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -133,7 +133,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module', // Disabled due to dependency conflicts
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxt/postcss8',
     '@nuxtjs/tailwindcss',
